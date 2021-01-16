d547ccc04f5784c89693845ba78d0ede251ec726
Khellermann Machado <khellermann@gmail.com>
2021-01-16 15:38:25 -0300

    Database seeding

--- backend/src/main/java/com/khellermann/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/khellermann/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.khellermann.dscatalog.entities.Category;
 import com.khellermann.dscatalog.repositories.CategoryRepository;
@@ -13,9 +14,10 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-
+	
+	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
-
 		return repository.findAll();
 	}
 
