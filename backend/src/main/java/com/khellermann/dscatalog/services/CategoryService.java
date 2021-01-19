15f8743863f3625e5bac00319c91eb8374d2be28
Khellermann Machado <khellermann@gmail.com>
2021-01-19 11:39:00 -0300

    Delete category

--- backend/src/main/java/com/khellermann/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/khellermann/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.khellermann.dscatalog.dto.CategoryDTO;
 import com.khellermann.dscatalog.entities.Category;
 import com.khellermann.dscatalog.repositories.CategoryRepository;
+import com.khellermann.dscatalog.services.exceptions.DatabaseException;
 import com.khellermann.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -53,6 +56,16 @@ public class CategoryService {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
 
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }
