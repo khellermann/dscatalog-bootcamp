1f68456e366093122edc19b3b2bab154c870e0c6
Khellermann Machado <khellermann@gmail.com>
2021-02-03 11:22:50 -0300

    Created catalog page

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import ProductCard from '../../core/components/ProductCard';
+import './styles.scss';
 
 
 const Catalog = () =>(
-    <div>
-        <h1>Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">
+            Catalog
+        </h1>
+        <div className="catalog-products">
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             <ProductCard />
+             
+
+        </div>
     </div>
 );
 
